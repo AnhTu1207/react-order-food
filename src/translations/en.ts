aa134bf321571312a9930afdd2e3edba6609ef95
phankiet110901 <phankiet110901@gmail.com>
2021-08-16 21:45:50 +0700

    add ui login page

---
+++ src/translations/en.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  login: {
+    button_title: "Login",
+    email_input_placeholder: "Email",
+    password_input_placeholder: "Password",
+    title: "Login",
+    sub_text: "Don't have an account?",
+    sign_up_link: "Sign up"
+  },
+};
