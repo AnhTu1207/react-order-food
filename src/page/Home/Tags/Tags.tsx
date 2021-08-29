26edfbdad98dcb1f6a81837fe1d3327511de6997
phankiet110901 <phankiet110901@gmail.com>
2021-08-29 19:11:49 +0700

    fix

--- src/page/Home/Tags/Tags.tsx
+++ src/page/Home/Tags/Tags.tsx
@@ -1,18 +1,102 @@
-import { FC } from "react";
-import { map } from "lodash";
+import React from 'react';
+import { makeStyles, Theme, useTheme } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import SwipeableViews from 'react-swipeable-views';
+import { TagsList } from 'assets';
+import {map} from 'lodash';
+import { TapStyled, Wrapper } from './styles';
+interface TabPanelProps {
+  children?: React.ReactNode;
+  dir?: string;
+  index: any;
+  value: any;
+}
 
-import { Wrapper, ChipStyled } from "./styles";
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
 
-import { TagsList } from "assets";
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
 
-const Tags: FC = () => {
+function a11yProps(index: any) {
+  return {
+    id: `full-width-tab-${index}`,
+		'aria-controls': `full-width-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+    width: 500,
+	},
+	indicator: {
+		background: "blue",
+    color: "#FFF",
+	}
+}));
+
+export default function FullWidthTabs() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index: number) => {
+    setValue(index);
+  };
+
   return (
     <Wrapper>
-      {map(TagsList, (item, index) => (
-        <ChipStyled label={item.name} clickable key={index} />
-      ))}
+      <div>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          textColor="primary"
+          variant="fullWidth"
+					aria-label="full width tabs example"
+					classes={{
+						indicator: classes.indicator
+					}}>
+					{map(TagsList, (item, index) => (
+						<TapStyled label={item.name} {...a11yProps(index)}  key={index} />
+					))}
+        </Tabs>
+      </div>
+      <SwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={value}
+        onChangeIndex={handleChangeIndex}
+      >
+        <TabPanel value={value} index={0} dir={theme.direction}>
+          Item One
+        </TabPanel>
+        <TabPanel value={value} index={1} dir={theme.direction}>
+          Item Two
+        </TabPanel>
+        <TabPanel value={value} index={2} dir={theme.direction}>
+          Item Three
+        </TabPanel>
+      </SwipeableViews>
     </Wrapper>
   );
-};
-
-export default Tags;
+}
--- src/page/Home/Tags/styles.tsx
+++ src/page/Home/Tags/styles.tsx
@@ -1,14 +1,14 @@
 import { styled } from "@material-ui/styles";
-import { Chip } from "@material-ui/core";
+import {  Tab } from "@material-ui/core";
 
 export const Wrapper = styled("div")({
-  overflowX: "auto",
+	overflowX: "auto",
   display: "flex",
   padding: "6px 0 12px 0",
   marginBottom: 16,
   "&::-webkit-scrollbar": {
-    width: 3,
-    height: 3,
+    width: "100%",
+    height: 5,
   },
   "&::-webkit-scrollbar-thumb": {
     background: "#e0e0e0",
@@ -16,20 +16,11 @@ export const Wrapper = styled("div")({
   
 });
 
-export const ChipStyled = styled(Chip)({
+export const TapStyled = styled(Tab)({
   background: "transparent",
   color: "var(--color-primary)",
   border: "1px solid var(--color-button-hover)",
-  padding: 2,
   fontSize: 14,
   marginRight: 10,
   borderRadius: 6,
-  "&:hover": {
-    background: "var(--color-primary)",
-    color: "#FFF",
-  },
-  "&:focus": {
-    backgroundColor: "var(--color-primary)",
-    color: "#FFF",
-  },
 });
