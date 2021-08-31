70d8f6ea512311da2eaff920fab4d80678d4675c
nhuna <nhuainguyen211@gmail.com>
2021-08-31 11:56:56 +0700

    fix action slidebar

--- src/page/Home/Tags/Tags.tsx
+++ src/page/Home/Tags/Tags.tsx
@@ -1,103 +1,31 @@
-import React from 'react';
-import { makeStyles, Theme, useTheme } from '@material-ui/core/styles';
-import Tabs from '@material-ui/core/Tabs';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import SwipeableViews from 'react-swipeable-views';
-import { TagsList } from 'assets';
-import {map} from 'lodash';
-import { TapStyled, Wrapper } from './styles';
-interface TabPanelProps {
-  children?: React.ReactNode;
-  dir?: string;
-  index: any;
-  value: any;
-}
+import { FC, useState } from "react";
+import { map } from "lodash";
 
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
+import { Wrapper, ChipStyled, useStyles } from "./styles";
 
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`full-width-tabpanel-${index}`}
-      aria-labelledby={`full-width-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
+import { TagsList } from "assets";
 
-function a11yProps(index: any) {
-  return {
-    id: `full-width-tab-${index}`,
-		'aria-controls': `full-width-tabpanel-${index}`,
-  };
-}
-
-const useStyles = makeStyles((theme: Theme) => ({
-  root: {
-    backgroundColor: theme.palette.background.paper,
-    width: 500,
-	},
-	indicator: {
-		background: "var(--color-primary)",
-    
-	},
-}));
-
-export default function FullWidthTabs() {
+const Tags: FC = () => {
   const classes = useStyles();
-  const theme = useTheme();
-  const [value, setValue] = React.useState(0);
+  const [isClick, setIsClick] = useState(null);
 
-  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
-    setValue(newValue);
-  };
-
-  const handleChangeIndex = (index: number) => {
-    setValue(index);
-  };
+  const handleOnClick = (id: any) => {
+    setIsClick(id)
+  }
 
   return (
     <Wrapper>
-      <div>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          
-          variant="fullWidth"
-					aria-label="full width tabs example"
-					classes={{
-						indicator: classes.indicator
-			
-					}}>
-					{map(TagsList, (item, index) => (
-						<TapStyled label={item.name} {...a11yProps(index)}  key={index} />
-					))}
-        </Tabs>
-      </div>
-      <SwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={value}
-        onChangeIndex={handleChangeIndex}
-      >
-        <TabPanel value={value} index={0} dir={theme.direction}>
-          
-        </TabPanel>
-        <TabPanel value={value} index={1} dir={theme.direction}>
-          
-        </TabPanel>
-        <TabPanel value={value} index={2} dir={theme.direction}>
-
-        </TabPanel>
-      </SwipeableViews>
+      {map(TagsList, (item, index) => (
+        <ChipStyled
+        label={item.name}
+        onClick={() => handleOnClick(index)}
+        clickable
+        key={index}
+        className={isClick == `${index}` ? classes.onActive : ""}
+        />
+      ))}
     </Wrapper>
   );
-}
+};
+
+export default Tags;
--- src/page/Home/Tags/styles.tsx
+++ src/page/Home/Tags/styles.tsx
@@ -1,26 +1,55 @@
-import { styled } from "@material-ui/styles";
-import {  Tab } from "@material-ui/core";
+import {
+  createStyles,
+  Theme,
+  makeStyles,
+  styled,
+} from "@material-ui/core/styles";
+
+import { Chip } from "@material-ui/core";
 
 export const Wrapper = styled("div")({
-	overflowX: "auto",
+  overflowX: "auto",
   display: "flex",
   padding: "6px 0 12px 0",
   marginBottom: 16,
   "&::-webkit-scrollbar": {
-    width: "100%",
-    height: 5,
+    width: 3,
+    height: 3,
   },
   "&::-webkit-scrollbar-thumb": {
     background: "#e0e0e0",
   }
-  
+
 });
 
-export const TapStyled = styled(Tab)({
+export const ChipStyled = styled(Chip)({
   background: "transparent",
   color: "var(--color-primary)",
   border: "1px solid var(--color-button-hover)",
+  padding: 2,
   fontSize: 14,
   marginRight: 10,
   borderRadius: 6,
+  "&:hover": {
+    background: "var(--color-primary)",
+    color: "#FFF",
+  },
+  "&:focus": {
+    backgroundColor: "var(--color-primary)",
+    color: "#FFF",
+  },
 });
+
+export const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    onActive:{
+      background: "var(--color-primary)",
+      color: "#FFF",
+            "&:focus": {
+    backgroundColor: "var(--color-primary)",
+    color: "#FFF",
+  },
+
+    }
+  })
+);
