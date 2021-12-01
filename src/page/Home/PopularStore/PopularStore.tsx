20b5ed434f7d409660a2be85ff831667183b8805
phankiet110901 <phankiet110901@gmail.com>
2021-12-02 00:19:35 +0700

    fix ui

--- src/page/Home/PopularStore/PopularStore.tsx
+++ src/page/Home/PopularStore/PopularStore.tsx
@@ -1,17 +1,17 @@
 import { FC } from "react";
 import { Grid } from "@material-ui/core";
 
-import { useTranslations } from "hooks";
 import { BoxStore } from "components/Box";
-import { Title } from "./styles";
+import styles from "./styles";
+
 import { Stores } from "assets";
 
+
 const PopularStore: FC = () => {
-  const { i18n } = useTranslations();
+  const classes = styles();
 
   return (
-    <div>
-      <Title>{i18n.t("home_page.popular_store")}</Title>
+    <div className={classes.wrapper}>
       <Grid container spacing={4}>
         {Stores.map((store, index) => (
           <Grid item md={4} sm={6} xs={12} key={index}>
