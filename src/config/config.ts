7a69d6ff37b0f8949a7586da611aa5887b782f75
AnhTu1207 <dinhanhtu1207@gmail.com>
2021-12-21 21:55:02 +0700

    Fix APP_URL

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   API_URL: "https://food-delivery-guru.herokuapp.com",
-  APP_URL: "http://localhost:3000"
+  APP_URL: "https://foodguruapp.herokuapp.com"
 };
